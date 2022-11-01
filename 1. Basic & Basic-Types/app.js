ba476075f1082dad94d06e3e8f4c6d07bcece738
Sunghyun627 <kksscc4@gmail.com>
2022-11-01 17:20:57 +0900

    변수에 할당된 Value의 type을 이용한 type infer

--- 1. Basic & Basic-Types/app.js
+++ 1. Basic & Basic-Types/app.js
@@ -1,8 +1,12 @@
 function add(n1, n2) {
-  return n1 + n2;
+    return n1 + n2;
 }
 // const number1 = '5';
 var number1 = 5;
 var number2 = 2.8;
 // Error 2345 : Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(add(number1, number2));
+var number3 = 5;
+// Error 2322 : Type 'string' is not assignable to type 'number'.
+// number3 = '3';
+number3 = 3;
