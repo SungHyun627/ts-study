ba592928cbad0d2617dcc2c5d04c8e88d87bfa61
Sunghyun627 <kksscc4@gmail.com>
2022-11-01 17:43:15 +0900

    Object 타입

--- 1. Basic & Basic-Types/app.js
+++ 1. Basic & Basic-Types/app.js
@@ -1,3 +1,4 @@
+/* Type Useage */
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -6,7 +7,26 @@ var number1 = 5;
 var number2 = 2.8;
 // Error 2345 : Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(add(number1, number2));
+/* Type Infer */
 var number3 = 5;
 // Error 2322 : Type 'string' is not assignable to type 'number'.
 // number3 = '3';
 number3 = 3;
+/* Object Types */
+var person1 = {
+    name: 'Max',
+    age: 30
+};
+// Property 'surName' does not exist on type '{ name: string; age: number; }'.
+// console.log(person1.surName);
+var person2 = {
+    name: 'Max',
+    age: 30
+};
+var person3 = {
+    name: 'Max',
+    age: 30
+};
+console.log(person3);
+var person4 = { name: 'Max', age: 30 };
+console.log(person4);
--- 1. Basic & Basic-Types/app.ts
+++ 1. Basic & Basic-Types/app.ts
@@ -1,3 +1,4 @@
+/* Type Useage */
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
@@ -9,7 +10,37 @@ const number2 = 2.8;
 // Error 2345 : Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(add(number1, number2));
 
+/* Type Infer */
+
 let number3 = 5;
 // Error 2322 : Type 'string' is not assignable to type 'number'.
 // number3 = '3';
 number3 = 3;
+
+/* Object Types */
+const person1 = {
+  name: 'Max',
+  age: 30,
+};
+
+// Property 'surName' does not exist on type '{ name: string; age: number; }'.
+// console.log(person1.surName);
+
+const person2: object = {
+  name: 'Max',
+  age: 30,
+};
+
+const person3: {} = {
+  name: 'Max',
+  age: 30,
+};
+
+console.log(person3);
+
+const person4: {
+  name: string;
+  age: number;
+} = { name: 'Max', age: 30 };
+
+console.log(person4);
