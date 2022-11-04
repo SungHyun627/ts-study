317efa7441883bef0858b6912628a22a5d2fec85
Sunghyun627 <kksscc4@gmail.com>
2022-11-04 13:59:59 +0900

    feat: use private access modifier to be accessible only in class

--- 3. Class & Interface/dist/app.js
+++ 3. Class & Interface/dist/app.js
@@ -1,14 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('accounting');
 accounting.describe();
-const accountingCopy = { name: 'Whatever', describe: accounting.describe };
-accountingCopy.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- 3. Class & Interface/src/app.ts
+++ 3. Class & Interface/src/app.ts
@@ -1,14 +1,31 @@
 class Department {
   name: string;
+  private employees: string[] = [];
+
   constructor(n: string) {
     this.name = n;
   }
+
   describe(this: Department) {
     console.log('Department: ' + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department('accounting');
 accounting.describe();
-const accountingCopy = { name: 'Whatever', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'Whatever', describe: accounting.describe };
+// accountingCopy.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// employees is private
+// accounting.employees[2] = 'Anna';
+accounting.printEmployeeInformation();
